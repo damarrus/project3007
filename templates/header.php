e61b219ce686c38cbf5ab0cd42af695f59f5835d
IBN93 <36645485+IBN93@users.noreply.github.com>
2018-11-15 21:01:09 +0300

    Свёрстан header
    Черновая версия

--- templates/header.php
+++ templates/header.php
@@ -9,5 +9,22 @@
 <body>
     <div class="wrapper">
         <header>
-
+            <div class="header-container">
+                <div class="header-container-logo">
+                    <img src="../images/logo.jpg" alt="shop-logo">
+                </div>
+                <nav>
+                    <a href="#">Женщинам</a>
+                    <a href="#">Мужчинам</a>
+                    <a href="#">Детям</a>
+                    <a href="#">Новинки</a>
+                    <a href="#">О нас</a>
+                </nav>
+                <div class="header-container-enter-and-bascet">
+                    <img src="../images/account.png" alt="enter-image">
+                    <a href="#">Войти</a>
+                    <img src="../images/bascet.png" alt="bascet-image">
+                    <a href="#">Корзина</a>
+                </div>
+            </div>
         </header>
