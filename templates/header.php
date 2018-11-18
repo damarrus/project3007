8cca891015fee551295a36f3084e21cdcf0e9715
IBN93 <36645485+IBN93@users.noreply.github.com>
2018-11-18 17:38:02 +0300

    Свёрстан header по макету

--- templates/header.php
+++ templates/header.php
@@ -9,9 +9,9 @@
 <body>
     <div class="wrapper">
         <header>
-            <div class="header-container">
+            <div class="header-container-nav">
                 <div class="header-container-logo">
-                    <img src="../images/logo.jpg" alt="shop-logo">
+                    <img src="../images/logo.jpg" alt="shop-logo" class="logo">
                 </div>
                 <nav>
                     <a href="#">Женщинам</a>
@@ -20,11 +20,12 @@
                     <a href="#">Новинки</a>
                     <a href="#">О нас</a>
                 </nav>
-                <div class="header-container-enter-and-bascet">
-                    <img src="../images/account.png" alt="enter-image">
-                    <a href="#">Войти</a>
-                    <img src="../images/bascet.png" alt="bascet-image">
-                    <a href="#">Корзина</a>
-                </div>
+            </div>
+            <div class="header-container-enter-and-bascet">
+                <img src="../images/account.png" alt="enter-image">
+                <a href="#">Войти</a>
+                <img src="../images/bascet.png" alt="bascet-image">
+                <a href="#">Корзина</a>
             </div>
         </header>
+        <hr>
