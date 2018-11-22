69f1fa960f291ed4510f513c473f99b2aedc900e
vinokurovayb <vinich1@mail.ru>
2018-11-22 21:09:29 +0300

    Начал делать модель для категории

---
+++ models/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once '../db.php';
+
+class Category
+{
+    private $id;
+    private $title;
+    private $description;
+
+    public function __construct($id)
+    {
+        global $mysqli;
+
+        $query = "SELECT title, description FROM categories WHERE category_id=$id";
+        $result = $mysqli->query($query);
+        $category_data = $result->fetch_assoc();
+
+        $this->id = $id;
+        $this->title = $category_data['title'];
+        $this->description = $category_data['description'];
+    }
+
+    public static function getAll()
+    {
+        global $mysqli;
+
+        $query = "SELECT category_id FROM categories";
+        $result = $mysqli->query($query);
+
+        $categories = [];
+        while ($category_data = $result->fetch_assoc()) {
+            $categories[] = new Category($category_data['category_id']);
+        }
+
+        return $categories;
+    }
+
+    INSERT INTO `categories` (`category_id`, `title`, `description`) VALUES
+    ,
+    ;
+}
