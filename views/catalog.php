caeda9d1834e795a9674e03cf8571b2e2ab160c2
1ciu <44919244+1ciu@users.noreply.github.com>
2018-11-22 19:57:00 +0300

    Начал верстать страницу

---
+++ views/catalog.php
@@ -0,0 +1,27 @@
+<?php require_once "../templates/header.php" ?>
+<link rel="stylesheet" href="../css/catalog.css">
+<main class="catalog-main">
+    <div class="catalog-category">ГЛАВНАЯ / МУЖЧИНАМ </div>
+    <h1 class="catalog-title">МУЖЧИНАМ</h1>
+    <div class="catalog-title-tovar">Все товары</div>
+    <div class="catalog-flex-select">
+        <select class="catalog-flex-select1">
+            <option>Куртки</option>
+            <option>Джинсы</option>
+            <option>Обувь</option>
+        </select>
+        <select class="catalog-flex-select1">
+            <option>M</option>
+            <option>L</option>
+            <option>XL</option>
+        </select>
+        <select class="catalog-flex-select1">
+            <option>0-1000 руб.</option>
+            <option>1000-3000 руб.</option>
+            <option>3000-6000 руб.</option>
+        </select>
+    </div>
+
+
+</main>
+<?php require_once "../templates/footer.php" ?>
