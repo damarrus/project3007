ca40ddc475062b17a8f6efd56085ec23ebfe009e
PISLEGi <1324332@mail.ru>
2018-12-03 14:30:30 +0300

    добавила обработчик

--- js/ajax_add_cart.js
+++ js/ajax_add_cart.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-    
-    $('#add').click(function(){
-        
+$(document).ready(function (){
+    var add = $('.goods-add-to-cart-button');
+    $('add').click(function (){
+        $.post('../controllers/add_to_cart.php', {id: $(this).attr('id')} );
 
     }); 
-       
-     
 });
